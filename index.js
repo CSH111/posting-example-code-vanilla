50cde7aeb272c979de65fb676acb54439bc594a4
CSH111 <seongho6022@naver.com>
2023-02-08 02:40:10 +0900

    throttling problem2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 잘못됨!
+
 const numberElem = document.querySelector(".number");
 const secondNumberElem = document.querySelector(".second-number");
 const buttonElem = document.querySelector(".add-button");
@@ -24,5 +26,6 @@ function handleSecondNumberClick() {
   throttle(() => add(secondNumberElem), 500);
 }
 
+// 두번째 이벤트가 작동하지 않는다.
 buttonElem.addEventListener("click", handleFirstNumberClick);
 buttonElem.addEventListener("click", handleSecondNumberClick);
